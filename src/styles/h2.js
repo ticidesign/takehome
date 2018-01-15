2f625e797fb4fa71a474c3f65ed2161741c01fdf
Ticiana Andrade <ticiana.andrade@lidaaustralia.com.au>
2018-01-15 18:28:42 +1100

    Updates

--- src/styles/h2.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-// Create a tile component that'll render a <h2> tag with some styles
-export default styled.h2`
-  font-family: "Merriweather Sans";
-  font-size: calc(1vw + 1vh);
-  font-weight: 400;
-  color: white;
-`;
--- src/styles/logo.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-// Create a tile component that'll render a <img> tag with some styles
-export default styled.img`
-  width: calc(2vw + 2vh);
-  height: auto;
-  margin-right: 20px;
-`;
