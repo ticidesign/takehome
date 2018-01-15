3c60afc659ca047f57a06c896fe048dd9184ca83
Ticiana Andrade <ticiana.andrade@lidaaustralia.com.au>
2018-01-15 18:53:27 +1100

    Fixing wrong commit

---
+++ src/styles/main.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+// Create a Wrapper component that'll render a <main> tag with some styles
+export default styled.main`
+  width: 100%;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+`;
